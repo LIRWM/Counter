19a065d95d2612de50f5eb24f227462f7ab951f4
LIRWM <a.isslamia@gmail.com>
2025-07-13 23:59:57 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Counter from './Counter';
+import Button from './Button';
 
-function App() {
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Counter count={count} />
+      <Button onClick={increment} label="Увеличить" />
+      <Button onClick={decrement} label="Уменьшить" />
     </div>
   );
-}
+};
 
 export default App;
